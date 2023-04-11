1a9883288b1fc2b80e84bafe54258b572e3d9a53
Carlos Henrique <ichfs9108@gmail.com>
2023-04-11 01:34:33 -0300

    create project

---
+++ f1-formula/src/main/java/com/f1/formula/one/series/F1FormulaApplication.java
@@ -0,0 +1,13 @@
+package com.f1.formula.one.series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1FormulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1FormulaApplication.class, args);
+	}
+
+}
---
+++ f1-formula/src/test/java/com/f1/formula/one/series/F1FormulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1.formula.one.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1FormulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

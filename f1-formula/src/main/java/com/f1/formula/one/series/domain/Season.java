9142545f0e09e6d6a98a461612707f226d0cea84
Carlos Henrique <ichfs9108@gmail.com>
2023-04-11 01:46:41 -0300

    Adicionando o domain - Season

---
+++ f1-formula/src/main/java/com/f1/formula/one/series/domain/Season.java
@@ -0,0 +1,36 @@
+package com.f1.formula.one.series.domain;
+
+import java.io.Serializable;
+
+public class Season implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String season;
+	private String url;
+
+	public Season(String season, String url) {
+		super();
+		this.season = season;
+		this.url = url;
+	}
+
+	public Season() {
+	}
+
+	public String getSeason() {
+		return season;
+	}
+
+	public void setSeason(String season) {
+		this.season = season;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
